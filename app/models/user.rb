e3f0dcde6e34c41ff98fb0280260f319cd79b3ce
tentamana <spectra626@icoud.com>
2020-10-07 11:31:30 +0900

    レビュー修正＋単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         
+        validates :nickname, presence: true
+        # validates :email, uniqueness: true, presence: true, inclusion: { in: ["@"] }
+        validates :name, presence: true, format: { with: /\A[\p{katakana}, {Hiragana}, {一-龠々}\p{blank}ー－]+\z/}
+        validates :last_name, presence: true, format: { with: /\A[\p{katakana}, {Hiragana}, {一-龠々}\p{blank}ー－]+\z/}
+        validates :name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+        validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX
+        validates :birthday, presence: true
+        
+        
+
+
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {"furima太郎"}
+    last_name {"山田"}
+    name {"太郎"}
+    last_name_kana{"ヤマダ"}
+    name_kana{"タロウ"}
+    email {"aa@gmail.com"}
+    birthday {Faker::Date.birthday}
+    password {"aa111111111"}
+    # password {password}
+    password_confirmation {password}
+  end
+end
