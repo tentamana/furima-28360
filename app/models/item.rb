07df26f2c64389b0592e07970540d22434782991
tentamana <spectra626@icoud.com>
2020-10-08 16:12:42 +0900

    validates修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,23 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :status, :category, :burden, :area, :shipment
+  belongs_to_active_hash :status
+  belongs_to_active_hash :category
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :shipment
   has_one_attached :item_image
+  belongs_to :user
+  # has_one :shopping
 
-   #空の投稿を保存できないようにする
-   validates :item_image, :item_name, :price, :description, :status, :category, :burden, :area, :shipment presence: true
-
-   #商品状態の選択が「--」の時は保存できないようにする
-   validates :status_id, numericality: { other_than: 1 }
-   #カテゴリーの選択が「--」の時は保存できないようにする
-   validates :category_id, numericality: { other_than: 1 } 
-   #配送料の選択が「--」の時は保存できないようにする
-   validates :burden_id, numericality: { other_than: 1 } 
-   #配送地域の選択が「--」の時は保存できないようにする
-   validates :area_id, numericality: { other_than: 1 } 
-   #配送日の選択が「--」の時は保存できないようにする
-   validates :shipment, numericality: { other_than: 1 } 
+   validates :item_image, presence: true  
+   validates :item_name, presence: true
+   validates :description, presence: true  
+   validates :price, presence: true,format: {with: /\A[0-9]+\z/} ,numericality: { only_integer: true,greater_than: 300, less_than: 9999999}
+   validates :status_id, presence:true, numericality: { other_than: 1 }
+   validates :category_id, presence: true, numericality: { other_than: 1 } 
+   validates :burden_id, presence: true, numericality: { other_than: 1 } 
+   validates :area_id, presence: true, numericality: { other_than: 1 } 
+   validates :shipment, presence: true, numericality: { other_than: 1 } 
 
 
 end
