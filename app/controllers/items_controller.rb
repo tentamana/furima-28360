4eed757660a2fed0d52cf01678b5f0d5c0bd4ea8
tentamana <spectra626@icoud.com>
2020-10-19 16:16:26 +0900

    コントローラーとルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-   before_action :authenticate_user!,only: [:new, :create]
+   before_action :authenticate_user!,only: [:new, :create, :edit, :update]
    def index
      @items = Item.all.order("created_at DESC")
    end
@@ -20,6 +20,17 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+       redirect_to item_path
+  end
+
 
   private
 
