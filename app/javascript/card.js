af1efb6ede03c5efc74b2fdc7f6f645e2ca94d0c
tentamana <spectra626@icoud.com>
2020-10-21 13:11:37 +0900

    api設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,23 @@
 const pay = ()=> {
-  Payjp.setPublicKey("pk_test_XXXXXXXX");
+  Payjp.setPublicKey("pk_test_ac07edc8e3568331dc4b239a");
   const form = document.getElementById("charge-form");
-  form.addEventListener("submit", () => {
-    console.log("イベント発火");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      console.log(status)
+      console.log(response)
+    })
   })
 }
 
